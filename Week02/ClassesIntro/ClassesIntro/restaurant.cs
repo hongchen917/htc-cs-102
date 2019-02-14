d3ba03f92f29379359351e2d11901d373a6e1f14
hongchen917 <chenhong045>
2019-02-13 19:31:12 -0500

    restau

---
+++ Week02/ClassesIntro/ClassesIntro/restaurant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ClassesIntro
+{
+    class restaurant
+    {
+        public string phone_number;
+        public string store_name;
+        public string order;
+        public string size;
+
+       public void tellname()
+        {
+            store_name = "hello";
+            MessageBox.Show(store_name);
+        }
+
+    }
+}
