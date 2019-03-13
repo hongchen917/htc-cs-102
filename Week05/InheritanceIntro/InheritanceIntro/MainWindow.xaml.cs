eb82030b2acea37b0403408fd13cf3b8ef6a799d
hongchen917 <chenhong045>
2019-03-13 19:29:57 -0400

    change image by button

--- Week05/InheritanceIntro/InheritanceIntro/MainWindow.xaml.cs
+++ Week05/InheritanceIntro/InheritanceIntro/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace InheritanceIntro
 {
@@ -61,7 +51,7 @@ namespace InheritanceIntro
 
         private void Quack_Button_Click(object sender, RoutedEventArgs e)
         {
-            
+            img.Source = new BitmapImage(new Uri("C:\\Users\\User1\\Source\\Repos\\htc-cs-102\\Week05\\InheritanceIntro\\InheritanceIntro\\duck.jpg"));
             Duck duck = new Duck();
             duck.Quack(); 
         }
