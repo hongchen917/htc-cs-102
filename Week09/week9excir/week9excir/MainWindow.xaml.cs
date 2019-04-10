736325e563af3fea6a7970057714595892a17e03
hongchen917 <chenhong045>
2019-04-10 19:27:20 -0400

    circle

---
+++ Week09/week9excir/week9excir/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace week9excir
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        EllipseGeometry myEllipseGeometry;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            myEllipseGeometry = new EllipseGeometry();
+            myEllipseGeometry.Center = new Point(200, 200);
+            myEllipseGeometry.RadiusX = 20;
+            myEllipseGeometry.RadiusY = 20;
+
+            Path myPath = new Path();
+            myPath.Fill = Brushes.SkyBlue;
+            myPath.Data = myEllipseGeometry;
+
+            myCanvas.Children.Add(myPath);
+        }
+
+        private void myCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+            PointAnimation myPointAnimation = new PointAnimation(e.GetPosition(this), TimeSpan.FromSeconds(1));
+            myPointAnimation.EasingFunction = new CircleEase();
+            myEllipseGeometry.BeginAnimation(EllipseGeometry.CenterProperty, myPointAnimation);
+        }
+    }
+}
